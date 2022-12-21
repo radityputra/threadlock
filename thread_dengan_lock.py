e260a3e85b361f3064c8342b15d2e6c58909202b
radit <radityaa.pr@gmail.com>
2022-12-21 15:08:55 +0700

    updated

--- thread_dengan_lock.py
+++ thread_dengan_lock.py
@@ -3,55 +3,56 @@ import urllib.request, urllib.parse, urllib.error
 
 class FetchUrls(threading.Thread):
     #Thread checking URLs.
-    def __init__(self, urls, output):
-    """
-    Contructor.
-    @param urls list of urls to check
-    @param output file to write urls ourtput
-    """
+    def __init__(self, urls, output, lock):
+        """
+        Contructor.
+        @param urls list of urls to check
+        @param output file to write urls ourtput
+        """ 
+        threading.Thread.__init__(self)
+        self.urls = urls
+        self.output = output
+        # add code here
+        self.lock = lock
+
+    def run(self):
+        #Thread run method. Check URLs one by one.
+        while self.urls:
+            url = self.urls.pop()
+
+            try:
+                d = urllib.request.urlopen(url)
+            except Exception as e:
+                print ("URL %s failed" %url)
+
+            self.lock.acquire()
+            print ('lock acquire by %s' % self.name)
+
+            self.output.acquire(d.read().decode())
+            print ('write done by %s' % self.name)
+
+            print ('lock released by %s' % self.name)
+            self.lock.release()
+
+            print('URL %s fetched by %s' % (url, self.name))
+
+    def main():
+        lock = threading.Lock()
     
-    threading.Thread.__init__(self)
-    self.urls = urls
-    self.output = output
-    # add code here
-    self.lock = lock
+        #list 1 of urls to fecth
+        urls1 = ['http://www/google.com', 'http://www.facebook.com']
+        #list 2 of urls to fecth
+        urls2 = ['http://www.yahoo.com', 'http://www.youtube.com']
+        f = open('output_not_correct.txt', 'w+', encoding='utf-8')
+        t1 = FetchUrls(urls1, f, lock)
+        t2 = FetchUrls(urls2, f, lock)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        f.close()
 
-def run(self):
-    #Thread run method. Check URLs one by one.
-    while self.urls:
-        url = self.urls.pop()
-
-        try:
-            d = urllib.request.urlopen(url)
-        except (urllib.error, e):
-            print ('URL %s failed: %s' %(url, e.reason))
-
-        self.lock.acquire()
-        print ('lock acquire by %s' %self.name)
-
-        self.lock.acquire(d.read().decode())
-        print ('write done by %s' %self.name)
-
-        print ('lock released by %s' %self.name)
-        self.lock.release()
-
-        print('URL %s fetched by %s' % (url, self.name))
-
-def main():
-    #list 1 of urls to fecth
-    urls1 = ['http://www/google.com', 'http://www.facebook.com']
-    #list 2 of urls to fecth
-    urls2 = ['http://www.yahoo.com', 'http://www.youtube.com']
-    f = open('output_not_correct.txt', 'w+')
-    t1 = FetchUrls(urls1, f)
-    t2 = FetchUrls(urls2, f)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-    f.close()
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
     
         
--- thread_tanpa_lock.py
+++ thread_tanpa_lock.py
@@ -2,39 +2,38 @@ import threading
 import urllib.request, urllib.parse, urllib.error
 
 class FetchUrls(threading.Thread):
-    #Thread checking URLs:
+    #Thread checking URLs.
     def __init__(self, urls, output):
         """
-
-        Contructor.
+        Constructor.
         @param urls list of urls to check
-        @param output file to write urls ourtput
+        @param output file to write urls output
         """
+        threading.Thread.__init__(self)
+        self.urls = urls
+        self.output = output
 
-    threading.Thread.__init__(self)
-    self.urls = urls
-    self.output = output
+    def run(self):
+        #Thread run method. Check URLs one by one.
+        while self.urls:
+            url = self.urls.pop()
 
-def run(self):
-    #Thread run method. Check URLs one by one.
-    while self.urls:
-        url = self.urls.pop()
+            try:
+                d = urllib.request.urlopen(url)
+            except Exception as e:
+                print ('URL %s failed' % url)
 
-        try:
-            d = urllib.request.urlopen(url)
-        except (urllib.error, e):
-            print('URL %s failed: %s' %(url, e.reason))
+            self.output.write(d.read().decode())
+            print ('write done by %s' % self.name)
+            print ('URL %s fetched by %s' % (url, self.name))
 
-        self.output.write(d.read().decode())
-        print ('write done by %s' % self.name)
-        print ('URL %s fetched by %s' % (url, self.name))
 
 def main():
-    #list 1 of urls to fecth
-    urls1 = ['http://www/google.com', 'http://www.facebook.com']
-    #list 2 of urls to fecth
+    # list 1 of urls to fetch
+    urls1 = ['http://www.mdp.cm', 'http://www.facebook.com']
+    # list 2 of urls to fetch
     urls2 = ['http://www.yahoo.com', 'http://www.youtube.com']
-    f = open('output_not_correct.txt', 'w+')
+    f = open('output_not_correct.txt', 'w+', encoding='utf-8')
     t1 = FetchUrls(urls1, f)
     t2 = FetchUrls(urls2, f)
     t1.start()
@@ -45,4 +44,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
